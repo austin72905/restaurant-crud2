b2a364c35c2c05bf35375b70a5c456e12267d504
Austin Lin <Linponggood@gmail.com>
2019-11-27 21:25:17 +0800

    Add Mongoose connection and create Restaurant model

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+//連線到mongodb
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+//載入models
+const Restaurant = require('./models/restaurant')
 
 app.get('/', (req, res) => {
   res.send('app')
