1ec28dc9bc9d0267c78d5920aa470218a9935cca
Austin Lin <Linponggood@gmail.com>
2019-11-28 15:40:23 +0800

    Add route

--- app.js
+++ app.js
@@ -17,9 +17,51 @@ db.once('open', () => {
 //載入models
 const Restaurant = require('./models/restaurant')
 
+
+
+
+//route
+//首頁
 app.get('/', (req, res) => {
   res.send('app')
 })
+
+//列出全部restaurant
+app.get('/restaurants', (req, res) => {
+  res.send('列出全部restaurant')
+})
+
+//新增restaurant 的頁面
+app.get('/restaurants/new', (req, res) => {
+  res.send('新增restaurant 的頁面')
+})
+
+//新增restaurant  -- >到 '列出全部restaurant的頁面'
+app.post('/restaurants', (req, res) => {
+  res.send('新增restaurant 的頁面')
+})
+
+//顯示detail 的頁面
+app.get('/restaurants/:id', (req, res) => {
+  res.send('顯示detail 的頁面')
+})
+
+//修改restaurant 的頁面
+app.get('/restaurants/:id/edit', (req, res) => {
+  res.send('新增restaurant 的頁面')
+})
+
+//修改restaurant
+app.post('/restaurants/:id/edit', (req, res) => {
+  res.send('修改restaurant')
+})
+
+//刪除restaurant
+app.post('/restaurants/:id/delete', (req, res) => {
+  res.send('刪除restaurant')
+})
+
+
 
 app.listen(3000, () => {
   console.log('the app is running on http://localhost:3000')
