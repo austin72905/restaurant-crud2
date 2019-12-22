fe27fdb6b828cf15490af05252dc35fd8c8eccb8
Austin Lin <Linponggood@gmail.com>
2019-12-22 15:32:37 +0800

    Add bcrypt to user registration route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 //登入頁面
 router.get('/login', (req, res) => {
@@ -43,12 +44,32 @@ router.post('/register', (req, res) => {
         name, email, password
       })
 
-      newUser.save()
-        .then(user => {
-          res.redirect('/')
+      // 先用 genSalt 產生「鹽」，第一個參數是複雜度係數，預設值是 10
+      bcrypt.genSalt(10, (err, salt) =>
+
+        // 再用 hash 把鹽跟使用者的密碼配再一起，然後產生雜湊處理後的 hash
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+
+          if (err) throw err
+          newUser.password = hash
+
+          // 用 bcrypt 處理密碼後，再把它儲存起來
+          newUser.save()
+            .then(user => {
+              res.redirect('/')
+            })
+            .catch(err => console.log(err))
+
+
         })
-        .catch(err => console.log(err))
+
+      )
+
     }
+
+
+
+
   })
 })
 
